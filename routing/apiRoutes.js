02fc0b9d41465243307171c3c440f188711d981d
Addis Casco <addis.casco@utexas.edu>
2018-04-27 11:18:48 -0500

    added modal functunality

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -8,10 +8,6 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         return res.json(friendData);
     });
-    //another route
-    // app.get("/api/survey", function(req, res){
-    //     console.log("COOL");
-    // }); 
     app.post("/api/friends", function (req, res) {
 
         var bestMatch = {
@@ -26,20 +22,20 @@ module.exports = function (app) {
 
         var totalDifference = 0;
 
-        for (var i = 0; i < friendsData.length; i++) {
-            console.log(friends[i]);
+        for (var i = 0; i < friendData.length; i++) {
+            console.log(friendData[i]);
             totalDifference = 0;
 
-            for (var j = 0; j < friends[i].scores[j]; j++) {
-                totalDifference += Match.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-                if (totalDifference <= bestMath.friendDifference) {
-                    bestMatch.name = friends[i].name;
-                    bestMatch.photo = friends[i].photo;
-                    bestMatcht.friendDifference = totalDifference;
+            for (var j = 0; j < friendData[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendData[i].scores[j]));
+                if (totalDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friendData[i].name;
+                    bestMatch.photo = friendData[i].photo;
+                    bestMatch.friendDifference = totalDifference;
                 }
             }
         }
-        friends.push(userData);
+        friendData.push(userData);
         res.json(bestMatch);
     });
 };
